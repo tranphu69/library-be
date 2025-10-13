8d5b599542d85e90256ee2464e9445ad45fad2ae
Trần Ngọc Phú <phu.tran@lifesup.local>
2025-10-13 16:39:28 +0700

    reset-libary

--- src/main/java/com/example/library/exception/BaseErrorCode.java
+++
@@ -1,9 +0,0 @@
-package com.example.library.exception;
-
-import org.springframework.http.HttpStatusCode;
-
-public interface BaseErrorCode {
-    int getCode();
-    String getMessage();
-    HttpStatusCode getStatusCode();
-}
